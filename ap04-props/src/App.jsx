dbac7873f7c77e2516ed81cad8daaecedad73aa2
Erick Diego <diegoerick2006@gmail.com>
2026-02-27 18:23:16 -0300

    lear: encerra codigo 2.5.4
    - grid system
    - solução quatro pedido
    - responsivididade foi atualizado

---
+++ ap04-props/src/App.jsx
@@ -0,0 +1,25 @@
+const App = () => (
+    <div className="container border mt-2">
+        <div className="row">
+            <div className="col-12">
+                <i className="fa fa-hippo fa-2x p-3"></i>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-sm-12 col-lg-6 col-xxl-3 border">
+                <div className="py-2">Pedido 1</div>
+            </div>
+            <div className="col-sm-12 col-lg-6 col-xxl-3 border">
+                <div className="py-2">Pedido 2</div>
+            </div>
+            <div className="col-sm-12 col-lg-6 col-xxl-3 border">
+                <div className="py-2">Pedido 3</div>
+            </div>
+            <div className="col-sm-12 col-lg-6 col-xxl-3 border">
+                <div className="py-2">Pedido 4</div>
+            </div>
+        </div>
+    </div>
+)
+
+export default App
---
+++ ap04-props/src/old.App.jsx
@@ -0,0 +1,29 @@
+const App = () => (
+  <div className="container border py-5">
+    <div className="row">
+      <div className="col-sm-12 col-md-6 col-lg-4">
+        <div className="p4 pg-primary"></div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-sm-12 col-md-6 col-lg-4 ">
+        <div className="p4 bg-onfo"></div>
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 ">
+        <div className="p4 bg-onfo"></div>
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4 ">
+        <div className="p4 bg-onfo"></div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-12">
+        <div className="p4 bg-sucess">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
